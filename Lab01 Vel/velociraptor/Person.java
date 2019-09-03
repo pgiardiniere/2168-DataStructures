a928ff7c91d8bd5ab274da26e1e43e09781e6a29
pgiardiniere <pgiardiniere@gmail.com>
2019-09-03 13:29:33 -0400

    person uses basic detection avoidance

--- Lab01 Vel/velociraptor/Person.java
+++ Lab01 Vel/velociraptor/Person.java
@@ -1,9 +1,41 @@
 public class Person extends Animal {
 
+    // instance vars
+    private boolean haveSeenVel = false;
+    private boolean canSeeVelNow = false;
+    private int distanceToVel;
+    private int directionToVel;
+    private int currentDirection;
+
 
     public Person(Model model, int row, int column) {
         super(model, row, column);
+        currentDirection = Model.random(Model.MIN_DIRECTION,
+                Model.MAX_DIRECTION);
     }
-    
-    int decideMove() { return random(Model.MIN_DIRECTION, Model.MAX_DIRECTION); }
+
+    // person movement, 1 move per turn
+    int decideMove() {
+        canSeeVelNow = false;
+
+        // Look in straight line down all 8 adjacent tiles to see things (velociraptor)
+        for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
+            if (look(i) == Model.VELOCIRAPTOR) {
+                canSeeVelNow = haveSeenVel = true;
+                directionToVel = i;
+                distanceToVel = distance(i);
+            }
+        }
+
+        // if seen velociraptor, try move out of line of sight:
+        if (canSeeVelNow) {
+            currentDirection = (directionToVel + 5) % 8;
+            return (currentDirection);
+        }
+        // if NO velociraptor in sight, stay (assures first sighting advantage)
+        else { return Model.STAY; }
+
+        // OLD / given return, just gives a random direction
+        // return random(Model.MIN_DIRECTION, Model.MAX_DIRECTION);
+    }
 }
