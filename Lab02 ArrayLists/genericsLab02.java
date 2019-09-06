9512387511e389dd53816846a89aebd4a3cb72b3
pgiardiniere <pgiardiniere@gmail.com>
2019-09-06 17:26:21 -0400

    Lab02 initial commit, setup required methods

---
+++ Lab02 ArrayLists/genericsLab02.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class genericsLab02 {
+    private static ArrayList foo = new ArrayList<String>();
+
+    public static void main(String args[]) {
+        unique(foo);
+        allMultiples(foo);
+        allStringsOfSize(foo);
+        tokenize(foo);
+        removeAll(foo);
+    }
+
+
+    public static void unique(ArrayList L) {
+
+    }
+
+    public static void allMultiples(ArrayList l) {
+
+    }
+
+    public static void allStringsOfSize(ArrayList l) {
+
+    }
+
+    public static void tokenize(ArrayList l) {
+
+    }
+
+    public static void removeAll(ArrayList l) {
+
+    }
+
+
+}
