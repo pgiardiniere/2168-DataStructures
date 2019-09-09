9e35ce83de1096e7929c6e93a83d6d380cf4cb5c
pgiardiniere <pgiardiniere@gmail.com>
2019-09-09 13:12:42 -0400

    better test lists + changes to unique()

--- Lab02 ArrayLists/genericsLab02.java
+++ Lab02 ArrayLists/genericsLab02.java
@@ -4,31 +4,40 @@ public class genericsLab02 {
     private static ArrayList foo = new ArrayList<String>();
 
     public static void main(String args[]) {
+
+        // make test data: ArrayList of Strings
+        List<String> bar = new ArrayList<>();
+        bar.add("poop");
+        bar.add("second");
+        bar.add("third");
+
+
         unique(foo);
         allMultiples(foo);
         allStringsOfSize(foo);
         tokenize(foo);
         removeAll(foo);
+
+
     }
 
-    // test whether any List entry is repeated in 
+    public static boolean isUnique(ArrayList l) {
+        return false;
+    }
+
+    // test whether any List element is repeated
     public static Boolean unique(ArrayList L) {
         for(int i = 0; i <= L.size(); i++) {
-            Object curEl = L.get(i);
             for (int j = 0; j <= L.size(); j++) {
-                if (j == i);  // do nothing on position curEl was pulled from
-                else {
-                    if (L.get(j).equals(L.get(i))) {
-                        return false;
-                    }
-                }
-            }
-            return true; // not positioned correctly, but this is the general idea
+                // if (elements equal to each other) and (not from same position) return true
+                if ( L.get(i).equals(L.get(j)) && (L.indexOf(i) != L.indexOf(j)) ) return true;
+            } 
         }
+        return false;
     }
 
     public static void allMultiples(ArrayList l) {
-
+        
     }
 
     public static void allStringsOfSize(ArrayList l) {
