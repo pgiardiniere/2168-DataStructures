2993f777c53463d1d50fed9bda0cf7af51cd450b
pgiardiniere <pgiardiniere@gmail.com>
2019-09-11 18:26:20 -0400

    lab02 finalize + cleanup

--- Lab02 ArrayLists/genericsLab02.java
+++ Lab02 ArrayLists/genericsLab02.java
@@ -3,43 +3,50 @@ import java.util.*;
 public class genericsLab02 {
 
     public static void main(String args[]) {
-        // populate first ArrayList of Integers
+        // make & populate first ArrayList of Integers
         ArrayList<Integer> intsOne = new ArrayList<Integer>();
         intsOne.add(1); intsOne.add(2); intsOne.add(3); intsOne.add(9); intsOne.add(33); intsOne.add(99);
 
-        // populate second ArrayList of Integers (for compare)
+        // make & populate second ArrayList of Integers -- rearranged for isPermutation()
         ArrayList<Integer> intsTwo = new ArrayList<Integer>();
-        intsTwo.add(1); intsTwo.add(2); intsTwo.add(3); intsTwo.add(9); intsTwo.add(33); intsTwo.add(99);
+        intsTwo.add(3); intsTwo.add(2); intsTwo.add(1); intsTwo.add(9); intsTwo.add(33); intsTwo.add(99);
 
-        // populate first ArrayList of Strings
+        // make & populate first ArrayList of Strings
         ArrayList<String> strsOne = new ArrayList<String>();
-        strsOne.add("foo"); strsOne.add("bar"); strsOne.add("foobar"); strsOne.add ("bar"); strsOne.add("fubar"); strsOne.add("this is a sentence");
+        strsOne.add("foo"); strsOne.add("bar"); strsOne.add("foobar"); strsOne.add ("bar"); strsOne.add("fubar");
     
-        // populate second ArrayList of Strings
+        // make & populate second ArrayList of Strings
         ArrayList<String> strsTwo = new ArrayList<String>();
-        strsTwo.add("foo"); strsTwo.add("bar"); strsTwo.add("foobar"); strsTwo.add ("bar"); strsTwo.add("fubar"); strsTwo.add("this is a sentence");
+        strsTwo.add("foo"); strsTwo.add("bar"); strsTwo.add("foobar"); strsTwo.add ("bar"); strsTwo.add("fubar");
 
+        // make strings
+        String str = "This string is a sentence.";
+        String extra = ".%wowee__- this? has@ some,. garbage";
 
-        // Aaaaaand now I see the issue. Cannot use across types without implementing generics. SEE: genericsLab02b.java
+        // execute all methods
+        unique(intsOne);
+        allMultiples(intsOne, 3);
+        allStringsOfSize(strsTwo, 3);
+        isPermutation(intsOne, intsTwo);
+        
+        // "str" Demonstrates base functionality, "extra" demonstrates extra credit functionality
+        tokenize(str);
+        tokenize(extra);
+
+        // after removing all "bar" elements from strsOne, it now is unique
         unique(strsOne);
         removeAll(strsOne, "bar");
-        unique(strsOne);
+        unique(strsOne);        
 
-        unique(intsOne);
-        // allMultiples();
-        // allStringsOfSize();
-        // tokenize();
-        // removeAll();
     }
 
-    public static Boolean unique(ArrayList<String> L) {
+    public static <E> Boolean unique(ArrayList<E> L) {
         for(int i = 0; i < L.size(); i++) {
             for (int j = 0; j < L.size(); j++) {
                 // if (2 els equal to each other) and (not comparing the same index location) return false
                 if ( L.get(i).toString().equals(L.get(j).toString()) && i != j ) return false;
             } 
         }
-        System.out.println("it's unique");
         return true;
     }
 
@@ -63,10 +70,10 @@ public class genericsLab02 {
         return allStrs;
     }
 
-    public static Boolean isPermutation(ArrayList<String> l1, ArrayList<String>l2) { 
+    public static <E> Boolean isPermutation(ArrayList<E> l1, ArrayList<E>l2) { 
         if (l1.size() != l2.size()) return false;        
         // make temp a copy of l2, to use remove() on.
-        ArrayList<String> temp = new ArrayList<String>();
+        ArrayList<E> temp = new ArrayList<E>();
         for (int i = 0; i < l2.size(); i++) {
             temp.add(l2.get(i));
         }
@@ -81,6 +88,7 @@ public class genericsLab02 {
 
     public static ArrayList<String> tokenize(String str) {
         str = str.strip();
+        str = str.replaceAll("[^a-zA-Z0-9 ]", "");  // extra credit: regex replaces all characters NOT a-zA-Z0-9 or whitespace with "" (i.e. nothing)
         String[] temp = str.split(" ");
         ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i < temp.length; i++) {
@@ -89,8 +97,8 @@ public class genericsLab02 {
         return list;
     }
 
-    public static void removeAll(ArrayList<Integer> l, Integer toKill) {
-        ArrayList<Integer> indices = new ArrayList<Integer>();  
+    public static <E> void removeAll(ArrayList<E> l, E toKill) {
+        ArrayList<E> indices = new ArrayList<E>();  
         for (int i = l.size()-1; i >= 0; i--) {
             if (l.get(i).equals(toKill)) indices.add(l.get(i));
         }
