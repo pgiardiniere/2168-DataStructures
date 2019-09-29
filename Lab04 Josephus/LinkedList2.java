2f43ad2b223e5352f03b6e7587bfd8853ccc8b29
pgiardiniere <pgiardiniere@gmail.com>
2019-09-28 21:04:16 -0400

    all LL2 class methods working + tested

--- Lab04 Josephus/LinkedList2.java
+++ Lab04 Josephus/LinkedList2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 @SuppressWarnings("unchecked")
 
-public class LinkedList2<E> { // implements Iterable<E>
+public class LinkedList2<E> implements Iterable<E>{  
     Node<E> head;
     int size;
 
@@ -23,48 +23,87 @@ public class LinkedList2<E> { // implements Iterable<E>
     }
 
     public void add(E dat) {        
+        Node newNode = new Node(dat);
         // walk to (almost) end of the list
         Node last = head;
-        for (int i = 1; i < size; i++) { 
-            last = last.next; 
-        }
-        // append newNode
-        Node newNode = new Node(dat);
+        for (int i = 1; i < size; i++) { last = last.next; }
+        // append newNode   
         last.next = newNode;
         newNode.next = head;
         size++;
     }
 
     public void add(int index, E dat) {
-
+        if (index < 0 || index >= size) { throw new IndexOutOfBoundsException(Integer.toString(index)); }
+        Node newNode = new Node(dat);
+        // place new node in head
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+            size++;
+        }
+        // place new node at end
+        else if (index == size-1) { add(dat); }
+        // place node in middle
+        else {
+            Node traverser = head;
+            for (int i = 1; i < index; i++) { traverser = traverser.next; }
+            newNode.next = traverser.next;
+            traverser.next = newNode;
+            size++;
+        }
+        
     }
 
-    public E remove(int index) {
-        return null;
+    public Node remove(int index) {
+        if (index < 0 || index >= size) { throw new IndexOutOfBoundsException(Integer.toString(index)); }        
+        Node temp = head;
+        // remove node at head
+        if (index == 0) {    
+            head = head.next;
+            size--;
+            return temp;
+        }
+        // remove node at end
+        else if (index == size-1) {
+            for (int i = 1; i < index; i++) { temp = temp.next; }
+            temp.next = head;
+            size--;
+            return temp;
+        }
+        // remove node in middle
+        else {
+            for (int i = 1; i < index; i++) { temp = temp.next; }
+            temp.next = temp.next.next;
+            size--;
+            return temp;
+        }
     }
 
     public String toString() {        
         if (size == 1) { return head.data.toString(); }
+        
         else {
             Node currentNode = head;
             StringBuilder strb = new StringBuilder();
-            do {
+            for (int i = 0; i < size; i++) {
                 strb.append(currentNode.data + " ==> ");
                 currentNode = currentNode.next;
-
-            } while (currentNode != head);
+            }
             return strb.toString();
         }
     }
 
     public static void main(String[] args) {
-        //              Testing: use preset data
+
+
+        //              JOSEPHUS: preset data mode.
         int numSoldiers = 5; 
         int numSkips = 2;
         int[] soldiers = new int[5];
         for (int i=0; i < numSoldiers; i++) { soldiers[i] = i + 1; }
 
-        /*              Production: Get user input. This stuff works - just tested it. uncomment when ready
+        /*              JOSEPHUS: user-input mode.
         // Get soldiers[] and interval
         Scanner inScan = new Scanner(System.in);
         System.out.println("input num soldiers, hit enter, input num skips");
@@ -74,17 +113,49 @@ public class LinkedList2<E> { // implements Iterable<E>
         for (int i = 0; i < numSoldiers; i++) { soldiers[i] = i + 1; }
         */
 
-        // Put soldiers[] into a single linked list. init loop at 1 as head already made
-        Node nod1 = new Node(1);                    //  FOR JSHELL LinkedList2.Node nod1 = new LinkedList2.Node(1);
+        Node nod1 = new Node(1);                    //  If in JSHELL:: LinkedList2.Node nod1 = new LinkedList2.Node(1);
         LinkedList2 sll = new LinkedList2(nod1);
         for (int i = 1; i < numSoldiers; i++) { 
-            System.out.println("index " + i + " soldier " +  soldiers[i]);
             sll.add(soldiers[i]); 
         }
 
         System.out.print("\n");
-        System.out.println(sll.head.data);
         System.out.println(sll.toString());
+
+        // ############### demonstrate all edge-cases ####################
+        // ############### addition ###############
+        // add at end
+        System.out.print("\n");
+        sll.add(numSoldiers - 1, 6);
+        System.out.println(sll.toString());
+        // add at head
+        sll.add(0, 9001);
+        System.out.println(sll.toString());
+        // add in middle
+        sll.add(3, 42);
+        System.out.println(sll.toString());
+
+        // ############### removal ###############
+        // remove at end
+        System.out.print("\n");
+        sll.remove(7);
+        System.out.println(sll.toString());
+        // remove at head 
+        sll.remove(0);
+        System.out.println(sll.toString());
+        // remove in middle
+        sll.remove(2);
+        System.out.println(sll.toString());
+
+
+        // demonstrate traversal & removal with iterator()
+        
+
+        // demonstrate removal utilizing iterator()
+
+
+
+        // ###############      end demonstration     ####################
     }
 
 
@@ -99,7 +170,6 @@ public class LinkedList2<E> { // implements Iterable<E>
         Node<E> prev;
         int index;
 
-        @SuppressWarnings("unchecked")
         public ListIterator() {
             nextItem = (Node<E>) head;
             index = 0;
