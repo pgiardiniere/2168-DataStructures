60d6581e00f9bdbfaa235a34408fc806521e272f
pgiardiniere <pgiardiniere@gmail.com>
2019-11-06 17:30:48 -0500

    begin lab08, maps + chaining

---
+++ Lab08 HashTables/Maps.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Maps {
+
+	// instance vars
+	// Map<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
+
+	// constructor
+	public Maps() {
+
+	}
+
+	public static void main(String[] args) {
+		// put instance var here for testing shenanigans
+		Map<Integer, ArrayList<String>> map = new HashMap<Integer, ArrayList<String>>();
+
+		// feed dummy data to map
+		// word-families (keys) are based on number of characters in word.
+		String[] strs = { "Wow", "this", "is", "an", "assignment"};
+		for (String str : strs) {
+
+			ArrayList<String> familyArr;
+			if (!map.containsKey(str.length())){
+				familyArr = new ArrayList<String>();
+				familyArr.add(str);
+			}
+			else { 
+				familyArr = map.get(str.length());
+				familyArr.add(str);
+			}
+
+			map.put(str.length(), familyArr);
+		}
+
+		System.out.println(map.keySet());
+		System.out.println(map.entrySet());
+	}
+
+}
