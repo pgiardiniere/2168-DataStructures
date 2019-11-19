4a8318c124b4d2da44a962deb602d78c394d873e
pgiardiniere <pgiardiniere@gmail.com>
2019-11-19 16:28:12 -0500

    insertSort works for lists Integers

--- Lab09 Sorting/InsertSortArray.java
+++ Lab09 Sorting/InsertSortArray.java
@@ -28,11 +28,46 @@ public class InsertSortArray {
 		return sorted;
 	}
 
+	public static List<Integer> insertSort(List<Integer> list) {
+		List<Integer> sorted = new ArrayList<Integer>(list.size());
+		if (list.size() == 0) return null;
+		else sorted.add(list.get(0));
+
+		for (int i = 1; i < list.size(); i++) {
+			for (int j = sorted.size(); j > -1; j--) {
+				sorted.add(list.get(i));
+				System.out.println(list.get(i) + " " + sorted.toString());
+				if (list.get(i) < sorted.get(j)) {
+					Integer tmp = sorted.get(j);
+					sorted.set(j, sorted.get(j-1));
+					sorted.set(j-1, tmp);
+				}
+			}
+		}
+		return sorted;
+	}
+
+	// need: 2 indeces. Recall: insertSort is IN-PLACE sorting algo
+	static void sort(List<Integer> list) {
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i-1) > list.get(i)) {
+				while (list.get(i-1) > list.get(i) && i != 0) {
+					Integer tmp = list.get(i);
+					list.set(i, list.get(i-1));
+					list.set(i-1, tmp);
+					i--;
+				}	
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		// ArrayList<> implementation
-		Integer[] tmp =  {0, 3, 2, 1, 55, 32, 4}; // {0, 1, 2, 3};
+		Integer[] tmp =  {0, 3, 2, 1, 55, 32, 4}; 
 		List<Integer> arr = new ArrayList<>(Arrays.asList(tmp));
 		
-		InsertSortArray.ArrayListSort(arr);
+		System.out.println(arr.toString());
+		InsertSortArray.sort(arr);
+		System.out.println(arr.toString());
 	}
 }
